639fcefc238b85f46ad0b770c5060d9bfed0b1e0
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-08-10 11:15:44 -0500

    test message

---
+++ RobovDino/RobovDino/Dinosaur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobovDino
+{
+    class Dinosaur
+    {
+        //member variables
+
+        Dinosaur dino;
+        string type;
+        int health;
+        int energy;
+        int attackPower;
+
+        //constructor
+
+        public Dinosaur(string type, int health, int energy, int attackPower)
+        {
+            Dinosaur wetnosaur = new Dinosaur("Wetnosaur", 100, 25, 150);
+            Dinosaur chomposaur = new Dinosaur("Chomposaur", 300, 40, 60);
+            Dinosaur kraken = new Dinosaur("Kraken", 150, 30, 120);
+        }
+        //member methods
+
+
+
+    }
+}
---
+++ RobovDino/RobovDino/Robot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobovDino
+{
+    class Robot
+    {
+        //member variables
+
+        Robot robot;
+        string name;
+        int health;
+        int powerLevel;
+        Weapon Weapon;
+
+        //constructor
+
+        public Robot(string name, int health, int powerLevel, Weapon weapon)
+        {
+            Robot hyperSpinningRobo = new Robot("Hyper Spinning Robo", 120, 80, weapon);
+            Robot atomicPowerRobot = new Robot("Atomic Power Robot", 300, 75, weapon);
+            Robot octobot = new Robot("Octobot", 140, 120, weapon);
+        }
+        //member methods
+
+
+    }
+}
